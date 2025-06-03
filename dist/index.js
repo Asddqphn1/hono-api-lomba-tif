028d7cca16ba20bb87e3eb18c96b2af1781d4323
Asddqphn1 <assiddiqiphn@gmail.com>
2025-06-03 10:41:45 +0700

    🔥 feat: deployment fix v1.9

--- dist/index.js
+++ dist/index.js
@@ -1,27 +1,29 @@
-import { Hono } from 'hono';
-
+import { Hono } from "hono";
+import auth from "./routes/auth";
+import register from "./routes/register";
+import login from "./routes/login";
+import juri from "./routes/juri";
 import daftarlomba from "./routes/daftarlomba";
 import daftarpeserta from "./routes/daftarpeserta";
-import juri from "./routes/juri";
-import login from "./routes/login";
-import logout from "./routes/logout";
-import penilaian from "./routes/penilaian";
-import register from "./routes/register";
-import sertifikat from "./routes/sertifikat";
 import submit from "./routes/submit";
 import users from "./routes/users";
-import auth from './routes/auth';
+import logout from "./routes/logout";
+import penilaian from "./routes/penilaian";
+import sertifikat from "./routes/sertifikat";
+
 const app = new Hono();
-app.route('/auth', auth);
-app.route('/register', register);
-app.route('/login', login);
-app.route('/juri', juri);
-app.route('/daftarlomba', daftarlomba);
-app.route('/daftarpeserta', daftarpeserta);
-app.route('/submit', submit);
-app.route('/users', users);
-app.route('/logout', logout);
-app.route('/penilaian', penilaian);
-app.route('/sertifikat', sertifikat);
-// Di backend (Hono)
+
+app.route("/auth", auth);
+app.route("/register", register);
+app.route("/login", login);
+app.route("/juri", juri);
+app.route("/daftarlomba", daftarlomba);
+app.route("/daftarpeserta", daftarpeserta);
+app.route("/submit", submit);
+app.route("/users", users);
+app.route("/logout", logout);
+app.route("/penilaian", penilaian);
+app.route("/sertifikat", sertifikat);
+
+// Ekspor aplikasi untuk digunakan di server
 export default app;
