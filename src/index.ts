f3484595d41469c5c963cf7d4478026091ed043d
Asddqphn1 <assiddiqiphn@gmail.com>
2025-06-03 09:52:28 +0700

    🔥 feat: deploymen v1.4

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import { Hono } from 'hono'
-import register from './routes/register'
 const app = new Hono()
 import login from './routes/login';
-import daftarlomba from './routes/daftarlomba';
+
 import daftarpeserta from './routes/daftarpeserta';
 import users from './routes/users';
 import juri from './routes/juri';
@@ -11,6 +10,8 @@ import logout from './routes/logout';
 import penilaian from './routes/penilaian';
 import submit from './routes/submit';
 import sertifikat from './routes/sertifikat';
+import register from 'routes/register';
+import daftarlomba from 'routes/daftarlomba';
 
 app.route('/auth', auth)
 app.route('/register', register)
