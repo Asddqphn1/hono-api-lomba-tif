b8619e889b8106dfc1eb927766796e3cf462a6dd
Asddqphn1 <assiddiqiphn@gmail.com>
2025-06-08 10:54:01 +0700

    🔥feat: adjust fix bug

--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -246,12 +246,11 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": null
+        "value": "postgresql://postgres:hfpIFeFsyxWvoIDSYxgTMGhVaweoWoiH@shortline.proxy.rlwy.net:48614/railway"
       }
     }
   },
